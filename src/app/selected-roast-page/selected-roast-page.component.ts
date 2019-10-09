a15d3913d2a28a6be576e7e543d43aa76f92308a
JP O'Grady <jpogrady@JPs-MacBook-Pro.local>
2019-10-09 14:27:45 -0400

    jp

--- src/app/selected-roast-page/selected-roast-page.component.ts
+++ src/app/selected-roast-page/selected-roast-page.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-selected-roast-page',
-  templateUrl: './selected-roast-page.component.html',
+  template: `
+  <h3>{{message}}</h3>`,
+  // I need to use {{}} to recieve the information from the parent comonent. 
   styleUrls: ['./selected-roast-page.component.css']
 })
 export class SelectedRoastPageComponent implements OnInit {
+
+// I am attempting to pass the data from the parent component(selectionPage) to the child component(selectedRoastPage). My resource says I need to use the @Input() with the child component and the   parentMessage = "message from parent" underneath the export class in the parent component. 
+
+  @Input() message: string;
 
   constructor() { }
 
--- src/app/selection-page/selection-page.component.ts
+++ src/app/selection-page/selection-page.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { CoffeeService } from '../coffee.service';
+import { SelectedRoastPageComponent } from '../selected-roast-page/selected-roast-page.component';
+import { ChildActivationEnd } from '@angular/router';
 
 
 export interface Choice {
@@ -10,11 +12,15 @@ export interface Choice {
 
 @Component({
   selector: 'app-selection-page',
-  templateUrl: './selection-page.component.html',
+  templateUrl: 'selection-page.component.html',
+
+  template: `<app-selected-roast-page [message] = 'roastResults' > </app-selected-roast-page>`,
+  //So I need to pass my message from the parent to child and I use [] to do so. 
+
   styleUrls: ['./selection-page.component.css']
 })
 export class SelectionPageComponent implements OnInit {
-roastResults: any;
+roastResults: any[];
 
   choices: Choice[] = [
     {value: 'light', viewValue: 'Light Roast'},
@@ -22,12 +28,22 @@ roastResults: any;
     {value: 'dark', viewValue: 'Dark Roast'}
   ];
 
+// @ViewChild(SelectedRoastPageComponent) 
+// so here I am attempting to use the @Input function call to pass data from selection page to selected roast page. This is the parent component. 
+ 
+
   constructor(private coffeeService: CoffeeService) { }
 
+  // message = (this.roastResults);
+  // Here I am sending the data set to roastResults to the child component(selectedRoastPage).
+
   ngOnInit() {
-    // this.searchRoast();
+    this.searchRoast('medium');
+    // We called the searchRoast function within the ngOnInit() function so that we could see if our data was being properly transfered. 
+    // this.roastResults = this.roastResults.data
   }
 
+  // Here we created the searchRoast function to fetch our data from the backend. We then set our data to the variable roastResults.
   searchRoast(roast: string): void {
     this.coffeeService.getRoast(roast).subscribe(data => {
       console.log(data);
@@ -35,14 +51,6 @@ roastResults: any;
       console.log(this.roastResults);
     });
   }
-  // click (click: Choice): void {
-  //   this.searchService.getResults(this.choices)
-  //   .subscribe(data => {
-  //     console.log(data);
-  //     this.click = data;
-  //     console.log(this.searchResults.results);
-  //   });
-  // }
 
 }
 
