938798479779a032821a6d447ec7b9b851c5788f
JP O'Grady <jpogrady@JPs-MacBook-Pro.local>
2019-10-10 14:29:05 -0400

    added cards for selected roast page

--- src/app/selected-roast-page/selected-roast-page.component.ts
+++ src/app/selected-roast-page/selected-roast-page.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CoffeeResult } from '../coffeeResults';
 @Component({
   selector: 'app-selected-roast-page',
   templateUrl: './selected-roast-page.component.html',
   styleUrls: ['./selected-roast-page.component.css']
 })
 export class SelectedRoastPageComponent implements OnInit {
+
+@Input() message: CoffeeResult;
+
   constructor() { }
   ngOnInit() {
+    this.runThis();
+  }
+
+  runThis(): void{
+    console.log(this.message);
   }
 }
