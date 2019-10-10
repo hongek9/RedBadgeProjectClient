35c2bf2e8ff697782d48fa092e927ad9d5f6cb71
Cody <chunt46511@gmail.com>
2019-10-10 14:33:58 -0400

    modal populates just not on screen

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(PaymentDialog, {
+      width: '250px'
+    });
+  }
 
   ngOnInit() {
   }
 
+}
+@Component({
+  selector: 'payment-dialog',
+  templateUrl: 'payment-dialog.html',
+})
+export class PaymentDialog{
+  constructor(
+    public dialogRef: MatDialogRef<PaymentDialog>,
+    ) {}
+
+    onNoClick(): void {
+      this.dialogRef.close();
+    }
 }
