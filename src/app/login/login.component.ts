39526d9aecde423c363d4328af88306749e32ba6
Ellie Hong <hongek9@gmail.com>
2019-10-11 10:53:07 -0400

    login/sign up token storage

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
   }
 
+  signInUser(testUser: User): void {
+    this.userService.signInUser(testUser).subscribe(data => {
+      console.log(data);
+      localStorage.setItem('token', data.sessionToken);
+    });
+  }
 }
