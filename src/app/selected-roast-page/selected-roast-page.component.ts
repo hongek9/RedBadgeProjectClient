160a31f419d29930a4c00519dd8fd805dece291b
Cody <chunt46511@gmail.com>
2019-10-14 11:53:52 -0400

    Modal for reviews added

--- src/app/selected-roast-page/selected-roast-page.component.ts
+++ src/app/selected-roast-page/selected-roast-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CoffeeResult } from '../coffeeResults';
+import { ReviewService } from '../review.service';
+import { Review } from '../review';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-selected-roast-page',
@@ -13,9 +16,13 @@ export class SelectedRoastPageComponent implements OnInit {
 
 @Input() message: CoffeeResult;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
   ngOnInit() {
     this.runThis();
+  }
+
+  openDialog(){
+    const dialogRef= this.dialog.open(ReviewDialog);
   }
 
   selectCoffee(coffee: any): void {
@@ -25,4 +32,44 @@ export class SelectedRoastPageComponent implements OnInit {
   runThis(): void{
     console.log(this.message);
   }
+  
 }
+
+@Component({
+  selector: 'review-dialog',
+  templateUrl: 'review-dialog.html',
+  styleUrls: ['review-dialog.css'],
+})
+
+export class ReviewDialog{
+  reviewResults: any;
+
+  constructor(private reviewService: ReviewService) { }
+
+
+
+    createReview(review: Review, coffeeId: number) {
+      this.reviewService.addReview(review, coffeeId).subscribe(data => {
+        console.log(data);
+      });
+    }
+  
+    findReviews(coffeId: number) {
+      this.reviewService.getReview(coffeId).subscribe(data => {
+          this.reviewResults = data;
+      });
+    }
+  
+    updateReview(review: Review, coffeId: number) {
+      this.reviewService.editReview(review, coffeId).subscribe(data => {
+        console.log(data);
+      });
+    }
+  
+    destroyReview(reviewId: number) {
+      this.reviewService.deleteReview(reviewId).subscribe(data => {
+        console.log(data);
+      });
+    }
+}
+
